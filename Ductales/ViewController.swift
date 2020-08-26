97e775e6ba9c41f61347db7b6efb9cbec0f3fce7
Dvs <sera.dvs@icloud.com>
2020-08-26 10:04:29 +0800

    Intial Commite
    i love yeet :)

---
+++ Ductales/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Ductales
+//
+//  Created by Dhiivyesh Rao on 19/8/20.
+//  Copyright © 2020 Dhiivyesh Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var BGCurved: UILabel!
+    @IBOutlet weak var whatToDoBtn: UIButton!
+    @IBOutlet weak var nearestShelterBtn: UIButton!
+    @IBOutlet weak var quizBtn: UIButton!
+    @IBOutlet weak var settingsBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Config Curved Corners
+        BGCurved.clipsToBounds = true
+        whatToDoBtn.clipsToBounds = true
+        nearestShelterBtn.clipsToBounds = true
+        quizBtn.clipsToBounds = true
+        settingsBtn.clipsToBounds = true
+        
+        BGCurved.layer.cornerRadius = 30
+        whatToDoBtn.layer.cornerRadius = 20
+        nearestShelterBtn.layer.cornerRadius = 20
+        quizBtn.layer.cornerRadius = 20
+        settingsBtn.layer.cornerRadius = 20
+    }
+
+
+}
+
---
+++ Ductales/WhatToDoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WhatToDoViewController.swift
+//  Ductales
+//
+//  Created by Dhiivyesh Rao on 26/8/20.
+//  Copyright © 2020 Dhiivyesh Rao. All rights reserved.
+//
+
+import UIKit
+
+class WhatToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
